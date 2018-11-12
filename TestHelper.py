0f8490b333f4d7b81596153e22807ed6400c0775
lucasoskorep <lucas.oskorep@gmail.com>
2018-11-12 16:12:38 -0600

    Added project baseline.

---
+++ TestHelper.py
@@ -0,0 +1,44 @@
+import linecache
+import os
+import tracemalloc
+import random
+
+def print_simple_trace(snapshot, key_type='lineno', limit=3):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+
+def bubble_sort(alist):
+    for passnum in range(len(alist) - 1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+
+def unsorted_list(length, min, max):
+    unsorted = []
+    for x in range(0, length):
+        unsorted.append(random.randint(min, max))
+    return unsorted
---
+++ tests.py
@@ -0,0 +1,7 @@
+import pytest
+import time
+from TestHelper import bubble_sort
+from TestHelper import print_simple_trace
+from TestHelper import unsorted_list
+
+
